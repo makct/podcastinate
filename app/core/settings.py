83890243a9cdb5fd175cea0f18ebdbe5844b2661
Maxim Terin <terin@reg.ru>
2020-09-24 21:25:36 +0400

    integration with nginx

--- app/core/settings.py
+++ app/core/settings.py
@@ -7,8 +7,8 @@ WEBHOOK_HOST = getenv("WEBHOOK_HOST", None)
 WEBHOOK_PORT = getenv("WEBHOOK_PORT", None)
 WEBHOOK_LISTEN = getenv("WEBHOOK_LISTEN", None)
 
-WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}:{WEBHOOK_PORT}"
-WEBHOOK_URL_PATH = f"/{API_TOKEN}/"
+WEBHOOK_URL_PATH = "/podcastinate/"
+WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}{WEBHOOK_URL_PATH}"
 
 WEBHOOK_SSL_CERT = "/opt/podcastinate/app/core/certs/webhook_cert.pem"
 WEBHOOK_SSL_PKEY = "/opt/podcastinate/app/core/certs/webhook_pkey.pem"
